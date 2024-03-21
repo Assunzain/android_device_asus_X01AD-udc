aadd1eb988df0872cd69a1f56cc7505172adeead
Yumi Yukimura <me.cafebabe@gmail.com>
2024-03-21 23:55:45 +0800

    X01AD: Switch to USB 1.3-basic HAL
    Change-Id: I6f477db2220b3563b44bb9545b7889166b8a6b06

--- device.mk
+++ device.mk
@@ -465,6 +465,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
 
+# USB HAL
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.3-service.basic
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.X01AD
