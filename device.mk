0b007231610fd1af565a306ad32c691dae782337
LuK1337 <priv.luk@gmail.com>
2024-03-21 08:51:02 +0100

    X01AD: Override fastboot-info.txt
    Change-Id: I1b293436b23eb8dfbc3a60759fee97cc73ba852d
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020-2021 The LineageOS Project
+# Copyright (C) 2019-2024 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -211,6 +211,9 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.X01AD
+
+# Fastboot
+TARGET_BOARD_FASTBOOT_INFO_FILE := $(LOCAL_PATH)/fastboot-info.txt
 
 # FM
 PRODUCT_PACKAGES += \
