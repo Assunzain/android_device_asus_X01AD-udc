3fffd84e8e1a285a9d501d783bb904b7d7f9f289
Michael Bestas <mikeioannina@gmail.com>
2024-06-15 00:50:24 +0300

    X01AD: Add a device specific FCM
    Change-Id: I9a36e9b33bf8c73cc0a98d441b141c3c698bc955
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -113,6 +113,7 @@ TARGET_QCOM_NO_FM_FIRMWARE := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
