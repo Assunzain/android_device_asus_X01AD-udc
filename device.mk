60fb6bde9a7fb94c32c4a7b8ae5753b5c08c32c0
whyredfire <whyredfire@gmail.com>
2024-10-06 01:25:40 +0000

    X01AD: Build libutils.vendor E vndksupport: Could not load libutils.so from sphal namespace: dlopen failed: library libutils.so not found.
    Signed-off-by: Drenzzz <naufalnazya@gmail.com>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- device.mk
+++ device.mk
@@ -472,6 +472,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v33.so
 
+PRODUCT_PACKAGES += \
+    libutils.vendor
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
