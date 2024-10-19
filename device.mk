6c048623d2d386ac880dc254b20234e953741c83
Wrdn28 <ifloraxd@gmail.com>
2024-10-19 11:58:36 +0700

    X01AD: build libprotobuf-cpp-full-vendorcompat
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- device.mk
+++ device.mk
@@ -368,6 +368,7 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
 # Protobuf
 PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-vendorcompat \
     libprotobuf-cpp-lite-vendorcompat \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
     libprotobuf-cpp-lite-3.9.1-vendorcompat
