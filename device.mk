db36e0567b609823e358b3444801243a26e51e57
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2024-10-30 17:45:05 +0700

    X01AD: build radio 1.4
    Unable to set property "ctl.interface_start" to "android.hardware.radio@1.4::IRadio/slot1": error code: 0x20
    
    Signed-off-by: Assunzain <asepsunanda.zain@gmail.com>

--- device.mk
+++ device.mk
@@ -404,7 +404,8 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.4.vendor \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
-    android.hardware.secure_element@1.2.vendor
+    android.hardware.secure_element@1.2.vendor \
+    android.hardware.radio.deprecated@1.0
 
 PRODUCT_PACKAGES += \
     librmnetctl \
