23b245059506f9c4fa75c5498da8c30c696fa30c
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2024-10-31 11:19:26 +0700

    fix

--- device.mk
+++ device.mk
@@ -406,8 +406,8 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.2.vendor \
-    android.hardware.radio.deprecated@1.0 /
-    android.hardware.radio.config@1.1.vendor /
+    android.hardware.radio.deprecated@1.0 \
+    android.hardware.radio.config@1.1.vendor \
     android.hardware.radio.config@1.1
 
 PRODUCT_PACKAGES += \
