fd714adbb8b6ab7404af5c49b3b6d8f7945a50b0
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2024-10-31 18:41:14 +0700

    Fix

--- device.mk
+++ device.mk
@@ -401,9 +401,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5 \
     android.hardware.radio@1.5.vendor \
-    android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
-    android.hardware.secure_element@1.2.vendor
+    android.hardware.secure_element@1.2.vendor \
+    android.hardware.radio.config@1.1.vendor
 
 PRODUCT_PACKAGES += \
     librmnetctl \
